8b55ce7d74c50cad9a99c40358fe45d57ed513e8
Shivam Chourasia <shivamchourasia15@gmail.com>
2023-01-02 23:59:17 +0530

    Upto Error Handling in auth

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,13 +1,22 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
   //private errors is equivalent to declaring error outside of constructor
   // and assigning values to its properties using this keyword.
 
   constructor(public errors: ValidationError[]) {
-    super();
+    // Only for logging purposes
+    super("Invalid request parameters");
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => {
+      return { message: err.msg, field: err.param };
+    });
   }
 }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +15,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// adding async breaks server
+app.all("*", async () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
