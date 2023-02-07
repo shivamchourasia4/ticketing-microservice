bdb82740e294c0bf046c9f3013a561ae5061506f
Shivam Chourasia <shivamchourasia004@gmail.com>
2023-02-07 06:04:05 +0530

    added tickets service

---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+
+  try {
+    // name of cluster IP service where we put domain
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!");
+  });
+};
+
+start();
